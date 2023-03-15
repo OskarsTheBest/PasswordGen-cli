f802a85536be3d235e3d6f21e130f0a3e9ac9ecf
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-03-15 17:23:11 +0200

    added complexity
    cargo run -- -l x -c y
    x is the number of symbold
    y is the complexity(low,medium,high)

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,48 @@ fn main() {
         .help("Sets the length of the password")
         .takes_value(true),
     )
+    .arg(
+        Arg::with_name("complexity")
+        .short('c')
+        .long("complexity")
+        .value_name("COMPLEXITY")
+        .help("Sets the complexity of the password (low, medium, high)")
+        .takes_value(true),
+    )
     .get_matches();
 
     let length: usize = matches.value_of("length").unwrap_or("12").parse().unwrap_or(12);
 
-    let password = generate_password(length);
+    let complexity: Complexity = match matches.value_of("complexity") {
+        Some("low") => Complexity::Low,
+        Some("medium") => Complexity::Medium,
+        Some("high") => Complexity::High,
+        _ => Complexity::Medium,
+    };
+
+    let password = generate_password(length, complexity);
     match password {
         Ok(p) => println!("Generated Password: {}",p),
         Err(e) => println!("Error: {}", e),
     }
 }
 
+#[derive(Debug)]
+enum Complexity{
+    Low,
+    Medium,
+    High,
+}
 
-fn generate_password(length: usize) -> Result<String, &'static str> {
+fn generate_password(length: usize, complexity: Complexity) -> Result<String, &'static str> {
     if length == 0 {
         return Err("Password length must be greater than zero");
     }
-    let chars: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                        abcdefghijklmnopqrstuvwxyz\
-                        0123456789)(*&^%$#@!~";
+    let chars: &[u8] = match complexity {
+        Complexity::Low => b"abcdefghijklmnopqrstuvwxyz",
+        Complexity::Medium => b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        Complexity::High => b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*&^%$#@!~",
+    };
     let mut rng = rand::thread_rng();
     Ok((0..length)
         .map(|_| char::from(chars[rng.gen_range(0..chars.len())]))
